109ff25a53a8682058e0bc1b610d4154d13d2020
Mike <magrozero@yandex.ru>
2020-01-24 11:48:32 +0300

    Add few components

---
+++ frontend/src/components/MainPage/MainPage.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './MainPage.css'
+
+class MainPage extends Component {
+  render() {
+    return (
+      <div>
+        MainPage
+      </div>
+    );
+  }
+}
+
+export default MainPage;
