57ff7724565263b69634868f422aa990e64508d9
Mike <magrozero@yandex.ru>
2020-01-28 11:04:52 +0300

    fix добавил редирект, если не залогинены

--- frontend/src/components/AddCompany/AddCompanyPage.jsx
+++ frontend/src/components/AddCompany/AddCompanyPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import CompanyList from "./CompanyList";
-import {withRouter} from "react-router-dom";
+import {withRouter, Redirect} from "react-router-dom";
 import CompanyDeatiils from "./CompanyDetails";
 
 const AddCompanyPage = ({history, match}) => {
@@ -10,6 +10,7 @@ const AddCompanyPage = ({history, match}) => {
 
   return (
     <div className="row mb2">
+      {!sessionStorage.user && <Redirect to={'/login'}/>}
       <div className="col-md-6">
         <CompanyList onCompanySelected={(id) => history.push(id)}/>
       </div>
