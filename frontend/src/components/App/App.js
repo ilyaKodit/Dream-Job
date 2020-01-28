98538020ec175b23fccda95226ed824fcfa8aafb
cruzador <batyrqh@protonmail.com>
2020-01-28 17:10:51 +0600

    страницы команды, еще будут иконки соц сетей

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -1,62 +1,58 @@
-import React, {Component} from 'react';
-import {BrowserRouter as Router, Link, Route, Switch} from 'react-router-dom';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Home from '../Home/Home';
 import Login from '../Login/Login';
-import Registration from "../Registration/Registration";
+import Registration from '../Registration/Registration';
 import FAQ from '../FAQ/FAQ';
 import Team from '../Team/Team';
 import Navbar from '../Navbar/Navbar';
 import AddCompanyPage from '../AddCompany/AddCompanyPage';
-import Feedback from "../Feedback/Feedback";
-import MainPage from "../MainPage/MainPage";
-import CompanyInfo from "../CompanyInfo/CompanyInfo";
-import Error from "../Error/Error";
-import {connect} from 'react-redux';
-
+import Feedback from '../Feedback/Feedback';
+import MainPage from '../MainPage/MainPage';
+import CompanyInfo from '../CompanyInfo/CompanyInfo';
+import Error from '../Error/Error';
+import { connect } from 'react-redux';
 
 class App extends Component {
+  render() {
+    return (
+      <div>
+        <Router>
+          <div>
+            <header>
+              <Navbar />
+            </header>
 
-    render() {
-        return (
-            <div>
-                <Router>
-                    <div>
-                        <header>
-                            <Navbar/>
-                        </header>
-
-                        {/*<hr/>*/}
-                        <Switch>
-
-                            <Route exact path="/" component={Home}/>
-                            <Route path="/login" component={Login}/>
-                            <Route path="/registration" component={Registration}/>
-                            <Route path="/faq" component={FAQ}/>
-                            <Route path="/team" component={Team}/>
-                            <Route path="/main" component={MainPage}/>
-                            <Route path="/company/:id" component={CompanyInfo}/>
-                            <Route path='/add/employer/:id?' component={AddCompanyPage}/>
-                            <Route path="/feedback/:id" component={Feedback}/>
-                            <Route component={Error}/>
-
-                        </Switch>
-                        <footer id="footer">
-                            {/*<hr/>*/}
-                            <ul id="horizontal-list">
-                                <li>
-                                    <Link to="/faq">FAQ</Link>
-                                </li>
-                                <li>
-                                    <Link to="/team">Team</Link>
-                                </li>
-                            </ul>
-                        </footer>
-                    </div>
-                </Router>
-            </div>
-        );
-    }
+            {/*<hr/>*/}
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/login" component={Login} />
+              <Route path="/registration" component={Registration} />
+              <Route path="/faq" component={FAQ} />
+              <Route path="/team" component={Team} />
+              <Route path="/main" component={MainPage} />
+              <Route path="/company/:id" component={CompanyInfo} />
+              <Route path="/add/employer/:id?" component={AddCompanyPage} />
+              <Route path="/feedback/:id" component={Feedback} />
+              <Route component={Error} />
+            </Switch>
+            <footer id="footer">
+              {/*<hr/>*/}
+              <ul id="horizontal-list">
+                <li>
+                  <Link to="/faq">FAQ</Link>
+                </li>
+                <li>
+                  <Link to="/team">Team</Link>
+                </li>
+              </ul>
+            </footer>
+          </div>
+        </Router>
+      </div>
+    );
+  }
 }
 
-export default connect()(App)
+export default connect()(App);
--- frontend/src/components/Team/Team.jsx
+++ frontend/src/components/Team/Team.jsx
@@ -1,7 +1,97 @@
 import React from 'react';
+import './Team.css';
+import { SocialIcon } from 'react-social-icons';
+
+import photo1 from './p1.png';
+import photo2 from './DSC07922 copy2.jpg';
+import photo3 from './p2.png';
+import photo4 from './p3.png';
 
 export default function Team() {
-  return <div>
-    here is team
-  </div>;
+  return (
+    <div className="team-section">
+      <div className="inner-width">
+        <h1>Meet Our Team</h1>
+        <div className="pers">
+          <div className="pe">
+            <img src={photo1} alt="" />
+            <div className="p-name">Full Name</div>
+            <div className="p-des">Designer</div>
+            <div className="p-sm">
+              <a href="#">
+                <i className="fab fa-facebook-f"></i>
+              </a>
+              <a href="#">
+                <i className="fab fa-twitter"></i>
+              </a>
+              <a href="#">
+                <i className="fab fa-instagram"></i>
+              </a>
+            </div>
+          </div>
+
+          <div className="pe">
+            <img src={photo2} alt="" />
+            <div className="p-name">Batyrkhan Zhaparov</div>
+            <div className="p-des">Бешеный разработчик</div>
+            <div className="p-sm">
+              <a href="https://twitter.com/home">
+                <SocialIcon
+                  network="twitter"
+                  bgColor="#ff5a01"
+                  style={{ height: 20, width: 20 }}
+                />
+                {/* <i className="fab fa-facebook-f"></i> */}
+              </a>
+              <a href="№">
+                <SocialIcon
+                  network="github"
+                  bgColor="#ff5a01"
+                  style={{ height: 20, width: 20 }}
+                />
+                {/* <i className="fab fa-twitter"></i> */}
+              </a>
+              {/* <a href="#">
+                <i className="fab fa-instagram"></i>
+              </a> */}
+            </div>
+          </div>
+
+          <div className="pe">
+            <img src={photo3} alt="" />
+            <div className="p-name">Full Name</div>
+            <div className="p-des">Developer</div>
+            <div className="p-sm">
+              <a href="#">
+                <i className="fab fa-facebook-f"></i>
+              </a>
+              <a href="#">
+                <i className="fab fa-twitter"></i>
+              </a>
+              <a href="#">
+                <i className="fab fa-instagram"></i>
+              </a>
+            </div>
+          </div>
+
+          <div className="pe">
+            <img src={photo4} alt="" />
+            <div className="p-name">Full Name</div>
+            <div className="p-des">Developer</div>
+            <div className="p-sm">
+              <a href="#">
+                <i className="fab fa-facebook-f"></i>
+              </a>
+              <a href="#">
+                <i className="fab fa-twitter"></i>
+              </a>
+              <a href="#">
+                <i className="fab fa-instagram"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
