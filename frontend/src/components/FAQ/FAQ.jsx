e10aa4664716d60d87d4d6d24b96d8d4e20d8e8d
cruzador <batyrqh@protonmail.com>
2020-01-29 23:54:05 +0600

    pulled FAQ

--- frontend/src/components/FAQ/FAQ.jsx
+++ frontend/src/components/FAQ/FAQ.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react';
+
+import Question from './Question';
+import Header from './Header.jsx';
+
+import './FAQ.css'
 
 export default function FAQ() {
+  const [faqs, setfaqs] = useState([
+    {
+      question: 'How many programmers does it take to screw in a lightbulb?',
+      answer: "None. We don't address hardware issues.",
+      open: true,
+    },
+    {
+      question: 'Who is the most awesome person?',
+      answer: 'You. The Viewer.',
+      open: false,
+    },
+    {
+      question:
+        'How many questions does it take to make a successful FAQ Page?',
+      answer: 'This many.',
+      open: false,
+    },
+  ]);
+
+  const toggleFAQ = index => {
+    setfaqs(
+      faqs.map((faq, i) => {
+        if (i === index) {
+          faq.open = !faq.open;
+        } else {
+          faq.open = false;
+        }
+
+        return faq;
+      }),
+    );
+  };
+
   return (
-    <div>
-      Here is FAQ
+    <div className="App">
+      <Header />
+      <div className="faqs">
+        {faqs.map((faq, i) => (
+          <Question faq={faq} index={i} toggleFAQ={toggleFAQ} />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
