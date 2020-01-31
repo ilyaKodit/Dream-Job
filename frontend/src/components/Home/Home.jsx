16061abde9a67abefbf197e42422f798e6e1e393
Mike <magrozero@yandex.ru>
2020-01-31 12:07:13 +0300

    fix Pre

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
 import './Home.css';
-// import cloudPic from './ic-Cloud-red.png';
+import cloudPic from './ic-Cloud-red.png';
 
 export default function Home() {
   return (
     <div className="home">
-      {/*<img className="cloudPic" src={cloudPic} alt="cloud" />*/}
+      <img className="cloudPic" src={cloudPic} alt="cloud" />
       <p className="sloganLine1 slogan">
         Заканчиваешь буткемп и не знаешь что дальше?
       </p>
