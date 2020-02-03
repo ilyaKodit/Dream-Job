8b00ae8b0ec91b9cbd639c756113f7003fc2813c
mike <m.avdeev@mail.ru>
2020-02-03 18:57:58 +0300

    heroku3

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const http = require('http');
 require('dotenv').config();
 const app = require('./app');
+
 const port = process.env.PORT || 5000;
 const server = http.createServer(app);
 server.listen(port);
